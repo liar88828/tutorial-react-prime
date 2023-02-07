52b7417a3600a644b89be86c1ac93663b8d86a15
brian <liar88828@gmail.com>
2023-02-07 19:07:09 +0700

    start

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Slidebar from "./components/Slidebar";
+import { DashBoard } from "./components/DashBoard";
+import styled from "styled-components";
+
+const Div = styled.div`
+
+
+
+`;
+
+function App() {
+  return (
+    <Div>
+      <Slidebar />
+      <DashBoard />
+    </Div>
+  );
+}
+
+export default App;
