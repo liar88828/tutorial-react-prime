53bd747e45a47b83a8f73e91da820af35e3e15f7
brian <liar88828@gmail.com>
2023-02-10 15:38:17 +0700

    make reusable

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -30,11 +30,35 @@ export const DashBoard = () => {
 
 
 const Section = styled.section`
-	margin-left:20vw;
-	padding:2rem;
-	height: 100%;
-	
-	
+  margin-left: 20vw;
+  padding: 2rem;
+  height: 100%;
+
+  .grid {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    gap: 1rem;
+    margin-top: 2rem;
+
+    .row__one {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      height: 50%;
+      gap: 1rem;
+    }
+
+    .row__two {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      height: 50%;
+      gap: 1rem;
+    }
+
+
+  }
+
+
 `
     
     
